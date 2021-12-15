74cfbffa7cbc3399ae29c8332fc95033ddc8631a
Halyna Romanyk <halya.romanyk@gmail.com>
2021-12-15 23:54:27 +0200

    My first commit

---
+++ src/com/cursor/Main.java
@@ -0,0 +1,23 @@
+package com.cursor;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double x, y, w, a, b,e, X;
+        a = 1;
+        b = 2;
+        e = 3;
+        x = -5;
+        y = 0;
+        w = -10;
+        X = 100L;
+
+        System.out.println(x+y-a/2*(x+w));
+        System.out.println(a*(x+x)/y-a*3);
+        System.out.println(a+b/x-y);
+        System.out.println(X-e/2%e+1);
+    }
+}
+

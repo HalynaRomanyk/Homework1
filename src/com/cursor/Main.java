80ad14d90196e11f48e86f983bc82f7bd900c8a7
Halyna Romanyk <C:\Users\Haliya\IdeaProjects\Homework1>
2021-12-16 20:49:42 +0200

    fixed bug

--- src/com/cursor/Main.java
+++ src/com/cursor/Main.java
@@ -1,23 +1,21 @@
 package com.cursor;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-	    double x, y, w, a, b,e, X;
+        double x, y, w, a, b, e, X;
         a = 1;
         b = 2;
         e = 3;
         x = -5;
         y = 0;
         w = -10;
-        X = 100L;
+        X = 100;
 
-        System.out.println(x+y-a/2*(x+w));
-        System.out.println(a*(x+x)/y-a*3);
-        System.out.println(a+b/x-y);
-        System.out.println(X-e/2%e+1);
+        System.out.println(x + y - a / 2 * (x + w));
+        System.out.println(a * (x + x) / y - a * 3);
+        System.out.println(a + b / x - y);
+        System.out.println(X - e / 2 % e + 1);
     }
 }
 
